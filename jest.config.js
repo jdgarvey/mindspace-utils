78a5b6bde9ef38e19caf621f079f069dd6dc60ea
Thomas Burleson <ThomasBurleson@Gmail.com>
2021-03-18 17:16:05 -0500

    fix(ci): add github action to build and test libs

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/libs/utils/react', '<rootDir>/libs/utils/react-akita'],
+  projects: ['<rootDir>/libs/utils/angular', '<rootDir>/libs/utils/react', '<rootDir>/libs/utils/react-akita'],
 };
--- libs/utils/angular/jest.config.js
+++ libs/utils/angular/jest.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   name: 'utils-angular',
-  preset: '../../../jest.config.js',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
   coverageDirectory: '../../../coverage/libs/utils/angular',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js'
-  ]
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
 };
